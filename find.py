b72ea668815682fa07852079dd5d929dd4fcb431
samuel ogundipe <ayoogundipe2005@gmail.com>
2017-10-22 15:01:13 +0100

    initial

---
+++ find.py
@@ -0,0 +1,21 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+data_list=[]
+site = requests.get('https://stackoverflow.com/');
+if site.status_code is 200:
+    content = BeautifulSoup(site.content, 'html.parser')
+    questions = content.find_all(class_='question-summary')
+    for question in questions:
+        topic = question.select( '.question-hyperlink')[0].get_text()
+        url = question.select( '.question-hyperlink')[0].get('href')
+        views = question.select('.views .mini-counts span')[0].get_text()
+        answers = question.select('.status .mini-counts span')[0].get_text()
+        votes = question.select('.votes .mini-counts span')[0].get_text()
+        new_data = {"topic": topic, "url": url, "views": views, "answers":answers, "votes":votes}
+        data_list.append(new_data)
+    with open ('find.csv','w') as file:
+        writer = csv.DictWriter(file, fieldnames = ["topic", "url", "views", "answers", "votes"], delimiter = ';')
+        writer.writeheader()
+        for row in data_list:
+            writer.writerow(row)
---
+++ readme.md
@@ -0,0 +1,36 @@
+Codebase for the tutorial on getting started with web scraping in Python
+
+**Python here refers to Pyhon3**
+## Installation:
+
+- Install required libraries using this command:
+```
+pip install -r requirements.txt
+```
+
+
+
+## Setting up and usage
+
+- clone this repo 
+
+```
+https://github.com/samuelayo/python_web_scraping.git
+```
+
+- change directory to this repo 
+```
+cd python_web_scraping
+```
+- run the css selector example
+```
+python css_selector.py
+
+```
+- run the `find` and `find_all` example
+```
+python find.py
+```
+
+
+
